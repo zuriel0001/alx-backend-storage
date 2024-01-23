165b7a178f740544304b77eee0fd9d7ce2eb6115
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-23 21:23:40 +0000

    fixed error

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,6 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-        """ Inserts a new document in a collection based on kwargs """
-            return (mongo_collection.insert(kwargs))
+    """Inserts a new document in a collection based on kwargs
+    """
+    return (mongo_collection.insert(kwargs))
