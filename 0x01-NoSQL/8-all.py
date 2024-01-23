80690e0ee5c55fd3ddc041b5ebd7bd741cdb60b1
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-23 21:12:10 +0000

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo
+"""
+
+
+def list_all(mongo_collection):
+    """ List all documents in collection """
+    documents = mongo_collection.find()
+
+    if documents.count() == 0:
+        return []
+
+    return (documents)
