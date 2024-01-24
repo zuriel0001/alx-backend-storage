920c882b23750e8ace94602de96d9c5af87524da
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-24 19:39:30 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-Redis module, Writing strings to Redis
-Reading from Redis and recovering original type
-Incrementing values, storing lists, Retrieving lists
+iA Redis module
+
+    To Write strings to Redis
+    To read from Redis and recover original type
+    To Increment values, store lists, Retrieve lists
 """
+
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -12,7 +15,7 @@ from functools import wraps
 
 def count_calls(method: Callable) -> Callable:
     """
-    Prototype: def count_calls(method: Caallable) -> Callable:
+    def count_calls(method: Caallable) -> Callable:
     Returns a Callable
     """
     @wraps(method)
@@ -24,7 +27,7 @@ def count_calls(method: Callable) -> Callable:
         key_m = method.__qualname__
         self._redis.incr(key_m)
         return method(self, *args, **kwds)
-    return wrapper
+    return (wrapper)
 
 
 def call_history(method: Callable) -> Callable:
@@ -45,14 +48,14 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(inp_m, data)
         fin = method(self, *args, **kwds)
         self._redis.rpush(outp_m, str(fin))
-        return fin
-    return wrapper
+        return (fin)
+    return (wrapper)
 
 
 def replay(func: Callable):
     """
     Prototype: def replay(func: Callable):
-    Displays history of calls of a particular function
+    Return the history of calls of a particular function
     """
     r = redis.Redis()
     key_m = func.__qualname__
@@ -60,10 +63,12 @@ def replay(func: Callable):
     outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
     calls_number = len(inp_m)
     times_str = 'times'
+
     if calls_number == 1:
         times_str = 'time'
     fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
     print(fin)
+
     for k, v in zip(inp_m, outp_m):
         fin = '{}(*{}) -> {}'.format(
             key_m, k.decode('utf-8'), v.decode('utf-8'))
@@ -72,12 +77,13 @@ def replay(func: Callable):
 
 class Cache():
     """
-    Store instance of Redis client as private variable _redis
+    Store the instance of Redis client as private variable _redis
     Flush the instance using flushdb
     """
     def __init__(self):
         """
         Prototype: def __init__(self):
+
         Store instance of Redis client as private variable _redis
         """
         self._redis = redis.Redis()
@@ -87,16 +93,16 @@ class Cache():
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store history of inputs and outputs for a particular function
+        Stores the  history of inputs and outputs for a particular function
         """
         gen = str(uuid.uuid4())
         self._redis.set(gen, data)
-        return gen
+        return (gen)
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
-        Convert data back to desired format
+        method to convert data back to desired format
         """
         value = self._redis.get(key)
         return value if not fn else fn(value)
